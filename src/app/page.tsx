3fb240c0d7431c263971e498baca3655563f90d8
greglobrigo <golivefun@gmail.com>
2023-10-25 22:52:39 +0800

    edited env.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,26 +14,25 @@ export default function Home() {
   const handleLogin = (e: any, p: any) => {
     axios({
       method: 'POST',
-      url: process.env.NEXT_PUBLIC_API_URL + 'login',
+      url: process.env.API_URL + 'login',
       data: {
         email: e,
         password: p
       },
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
       }
     })
-    .then((res) => {
-      if (res.data.status === 'success') {
-        setIsLoggedIn(true)
-        console.log(res.data)
-      } else {
-        setErrorMessage(res.data.message)
-      }
-    }).catch((err) => {
-      alert(err)
-    })
+      .then((res) => {
+        if (res.data.status === 'success') {
+          setIsLoggedIn(true)
+          console.log(res.data)
+        } else {
+          setErrorMessage(res.data.message)
+        }
+      }).catch((err) => {
+        alert(err)
+      })
   }
 
 
@@ -43,48 +42,48 @@ export default function Home() {
 
   return (
     <>
-    {
-      errorMessage && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-        <p>{errorMessage}</p>
-      </div>
-    }
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center mt-[100px]">
-        <h1 className="text-[#1e1e1e] text-8xl text-center leading-[0.8] pb-[50px] font-bold
-        ">
-          The Grid POC
-        </h1>
-        <h1 className="text-[#1e1e1e] text-7xl text-center leading-[0.8] pb-[50px] font-bold">
-          by
-        </h1>
-        <Image src="/mosaic-logo.png" width={500} height={500} alt="mosaic-logo" />
-
-        <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white mt-[200px]">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Email
-              </label>
-              <input name="email" onChange={(e) => setEmail(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-              </label>
-              <input name="password" onChange={(e) => setPassword(e.target.value)}
-                className="shadow appearance-none borde rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-            </div>
-            <div className="flex flex-col items-center justify-center mb-4 cursor-pointer">
-              <button onClick={() => handleLogin(email, password)}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4" type="button">
-                Log In
-              </button>
-            </div>
-          </form>
+      {
+        errorMessage && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <p>{errorMessage}</p>
         </div>
-      </div>
-    </main>
+      }
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center justify-center mt-[100px]">
+          <h1 className="text-[#1e1e1e] text-8xl text-center leading-[0.8] pb-[50px] font-bold
+        ">
+            The Grid POC
+          </h1>
+          <h1 className="text-[#1e1e1e] text-7xl text-center leading-[0.8] pb-[50px] font-bold">
+            by
+          </h1>
+          <Image src="/mosaic-logo.png" width={500} height={500} alt="mosaic-logo" />
+
+          <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white mt-[200px]">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Email
+                </label>
+                <input name="email" onChange={(e) => setEmail(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+              </div>
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Password
+                </label>
+                <input name="password" onChange={(e) => setPassword(e.target.value)}
+                  className="shadow appearance-none borde rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+              </div>
+              <div className="flex flex-col items-center justify-center mb-4 cursor-pointer">
+                <button onClick={() => handleLogin(email, password)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4" type="button">
+                  Log In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
