82291e87884001cc1bba1e32b1fb08d165a4d59c
greglobrigo <golivefun@gmail.com>
2023-10-27 10:45:14 +0800

    Adjusted height for laptop.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function Home() {
         </div>
       }
 
-      <main className="flex min-h-screen flex-col items-center justify-between pt-5 pr-20 pl-20">
+      <main className="flex min-h-screen flex-col items-center justify-between pr-20 pl-20">
         <div className="flex flex-col items-center justify-center xs:mt-[0] md:mt-[100px]">
           <h1 className="text-[#1e1e1e] xs:text-6xl md:text-8xl text-center leading-[0.8] xs:pb-[10px] md:pb-[25px] font-bold
         ">
@@ -176,7 +176,7 @@ export default function Home() {
           <Image src="/mosaic-logo.png" width={500} height={500} alt="mosaic-logo" />
           {
             !isLoggedIn &&
-            <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white xs:mt-[25px] md:mt-[200px]">
+            <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white xs:mt-[25px] md:mt-[25px]">
               <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
                 <div className="mb-4">
                   <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -205,7 +205,7 @@ export default function Home() {
 
           {
             isLoggedIn &&
-            <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white mt-[100px]">
+            <div className="w-full max-w-xs bg-gradient-to-r from-blue-600 via-blue-400 to-white mt-[50px]">
               <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
                 <div className="mb-4">
                   <label className="block text-gray-700 text-sm font-bold mb-2">
